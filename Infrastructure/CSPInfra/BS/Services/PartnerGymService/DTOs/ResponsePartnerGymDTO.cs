fef307163b86c51ac86bae5413b102d962b46838
muhdhasansami <hasansami806@gmail.com>
2025-11-26 21:15:37 +0500

    Final commit from hasan (tweaked getallgyms api).

--- Infrastructure/CSPInfra/BS/Services/PartnerGymService/DTOs/ResponsePartnerGymDTO.cs
+++ Infrastructure/CSPInfra/BS/Services/PartnerGymService/DTOs/ResponsePartnerGymDTO.cs
@@ -11,5 +11,9 @@
         public string status { get; set; }
         public double? latitude { get; set; }
         public double? longitude { get; set; }
+        public string address { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
     }
 }
